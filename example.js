b9ba213056850f71dfc6c9aa94f90060bfc16bbb
Juho <juho.lyytikainen@cs.tamk.fi>
2011-05-17 17:06:22 +0300

    gallery js file changed

--- example.js
+++ example.js
@@ -1,15 +1,23 @@
 $(document).ready( function() {
   // We dynamically add some nav buttons
-  var prevNext = '<div id="album-nav"><button class="prev">&laquo; Edellinen</button>' +
-                 ' <button class="next">Seuraava &raquo;</button></div>';
+  //var prevNext = '<div id="album-nav"><button class="prev">&laquo; Edellinen</button>' +
+  //               ' <button class="next">Seuraava &raquo;</button></div>';
+
+  var prevNext = '<div id="album-nav"><button class="prev">&laquo;</button>' +
+  '<span> Selaa albumeja </span><button class="next">&raquo;</button></div>';
+
+  var prev = '<div id="album-nav"><button class="prev">&laquo;</button>';
+  var next = '<button class="next">&raquo;</button></div>';
   
+  //$(prev).insertBefore("#albumname");
+  //$(next).insertAfter("#albumname");
   $(prevNext).insertAfter("#hgal");
   //$(prevNext).insertAfter('.album:last');
   // Add cycling between albums
   $('.album').wrapAll('<div id="photo-albums"></div>');
   $('#photo-albums').cycle({ 
     fx:     'turnDown', 
-    speed:  500, 
+    speed:  800, 
     timeout: 0, 
     next:   '.next', 
     prev:   '.prev' 
